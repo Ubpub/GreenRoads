e824e15b1cf13d6c6386c404e03d69a33af9268d
Cecilia Llamazares Lopez <ceciliallamazares99@gmail.com>
2023-01-17 14:24:35 +0100

    Landing page y parte de registro

--- js/header_footer.js
+++ js/header_footer.js
@@ -1,9 +1,12 @@
-const header_view = "./views/header.html";
+const header_view = "/views/included-views/header.html";
 let header_id = '#index-header';
-const footer_view = "./views/footer.html";
+const nav_bar = "/views/included-views/nav-bar.html";
+let nav_bar_id = '#nav-bar';
+const footer_view = "/views/included-views/footer.html";
 let footer_id = '#index-footer';
 
 obtenerHeaderFooter(header_view, header_id);
+// obtenerHeaderFooter(nav_bar, nav_bar_id);
 obtenerHeaderFooter(footer_view, footer_id);
 
 function obtenerHeaderFooter(view, id) {
