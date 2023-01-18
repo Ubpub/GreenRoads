def3eb649c38248a556cecb9e6483f089f06ebe1
Cecilia Llamazares Lopez <ceciliallamazares99@gmail.com>
2023-01-18 13:49:59 +0100

    Cambios en la LandingPage y Registro

---
+++ js/register.js
@@ -0,0 +1,16 @@
+
+renderPage();
+
+function renderPage() {
+    document.querySelector('#btn-registro').addEventListener('click', (e) => {
+        getNameSurname(document.querySelector('#nombreApell').value);
+        getuser(document.querySelector('#usuario').value);
+    });
+}
+
+function getNameSurname(texto) {
+
+    // Sugerir nombre de usuario en un futuro
+    /* [nombre, ...apellidos] = texto.split(' ');
+    apellidos = apellidos.join(' '); */
+}
